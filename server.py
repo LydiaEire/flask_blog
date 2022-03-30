7269fb8dc929e21972a88590a4fdd1335432f5fc
Your Name <you@example.com>
2022-03-30 12:42:10 +0300

    basic code

---
+++ server.py
@@ -0,0 +1,34 @@
+import requests as requests
+from flask import Flask, render_template
+import datetime
+
+from post import Post
+
+app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+posts = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    post_objects.append(post_obj)
+
+
+@app.route("/")
+def home():
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year=current_year)
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
