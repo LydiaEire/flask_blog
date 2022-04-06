49080861a838da5cba9867cdcb6de78fb077c14d
Your Name <you@example.com>
2022-04-06 12:07:56 +0300

    Fix the header and footer, using jinja include to render templates, make About/Contact pages work, render blog posts from an API

--- server.py
+++ server.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-posts = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+posts = requests.get('https://api.npoint.io/9ec6fac4764e429e0f8b').json()
 post_objects = []
 for post in posts:
     post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
     post_objects.append(post_obj)
 
-@app.route("/")
+
+@app.route('/')
 def home():
     current_year = datetime.datetime.now().year
-    return render_template("index.html", all_posts=post_objects, years=current_year)
+    return render_template('index.html', all_posts=post_objects, years=current_year)
 
 
 @app.route('/post/<int:index>')
@@ -28,6 +29,16 @@ def show_post(index):
         if blog_post.id == index:
             requested_post = blog_post
     return render_template('post.html', post=requested_post)
+
+
+@app.route('/about')
+def about_me():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact_me():
+    return render_template('contact.html')
 
 
 if __name__ == "__main__":
